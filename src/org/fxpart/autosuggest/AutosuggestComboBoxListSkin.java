b6ccd2ae21d624ff01191459c85ce694e38ed264
metairie <metairie@ebu.ch>
2015-07-07 10:12:34 +0200

    align all

--- src/org/fxpart/autosuggest/AutosuggestComboBoxListSkin.java
+++ src/org/fxpart/autosuggest/AutosuggestComboBoxListSkin.java
@@ -5,6 +5,8 @@ import com.sun.javafx.scene.control.behavior.KeyBinding;
 import com.sun.javafx.scene.control.skin.BehaviorSkinBase;
 import com.sun.javafx.scene.control.skin.ComboBoxListViewSkin;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
@@ -12,6 +14,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import java.util.Collections;
+import java.util.DoubleSummaryStatistics;
 
 /**
  * Created by metairie on 07-Jul-15.
@@ -23,8 +26,9 @@ public class AutosuggestComboBoxListSkin<T> extends BehaviorSkinBase<Autosuggest
      **************************************************************************/
 
     // visuals
-    private final VBox vroot;
-    private final HBox htexts;
+    private final HBox root;
+    private final VBox vtext;
+    private final VBox vcombo;
     private final ComboBox<T> comboBox;
     private final TextField selectedItem;
     private final ProgressBar loadingIndicator;
@@ -40,8 +44,9 @@ public class AutosuggestComboBoxListSkin<T> extends BehaviorSkinBase<Autosuggest
         super(control, new BehaviorBase<>(control, Collections.<KeyBinding>emptyList()));
         this.control = control;
         items = control.getItems();
-        vroot = new VBox();
-        htexts = new HBox();
+        root = new HBox();
+        vcombo = new VBox();
+        vtext = new VBox();
         loadingIndicator = new ProgressBar();
         selectedItem = new TextField("");
         comboBox = new ComboBox<T>(items) {
@@ -60,13 +65,25 @@ public class AutosuggestComboBoxListSkin<T> extends BehaviorSkinBase<Autosuggest
         comboBox.setEditable(true);
 
         // build control up
-        htexts.setPrefWidth(300);
-        htexts.getChildren().add(selectedItem);
-        htexts.getChildren().add(comboBox);
-        vroot.setPrefWidth(htexts.getWidth());
-        vroot.getChildren().add(loadingIndicator);
-        vroot.getChildren().add(htexts);
-        getChildren().add(vroot);
+        // Insets(double top, double right, double bottom, double left)
+        root.setStyle("-fx-background-color: #336699;");
+        root.setPadding(new Insets(1, 1, 1, 1));
+
+        vcombo.setStyle("-fx-background-color: #FFFFBB;");
+        vcombo.setPadding(new Insets(5, 5, 5, 5));
+        loadingIndicator.setMaxWidth(Double.MAX_VALUE);
+        vcombo.getChildren().add(loadingIndicator);
+        vcombo.getChildren().add(comboBox);
+
+        vtext.setStyle("-fx-background-color: #AAFFBB;");
+        vtext.setPadding(new Insets(10, 5, 5, 5));
+        selectedItem.setVisible(true);
+        selectedItem.setMaxHeight(Double.MAX_VALUE);
+        vtext.getChildren().add(selectedItem);
+
+        root.getChildren().add(vtext);
+        root.getChildren().add(vcombo);
+        getChildren().add(root);
     }
 
 }
